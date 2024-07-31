69963ed24d6c1e6e622f10056d4cf628eb23ab09
cyhunblr <ahmetceyhunbilir16@gmail.com>
2024-07-31 05:34:27 +0300

    add rectified image publisher and some other parameters

--- include/tis_ros.h
+++ include/tis_ros.h
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <string>
 
-#include "tiscamera_interface/tiscamera_interface.hpp"
+#include <tiscamera_interface/tiscamera_interface.hpp>
 
 #include <ros/ros.h>
 
@@ -22,17 +22,23 @@
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 
-TisCameraManager *cam = nullptr; // ?
-
-image_transport::CameraPublisher img_raw_pub_;
-image_geometry::PinholeCameraModel *pinhole_model_;
-camera_info_manager::CameraInfoManager *camera_info_manager_;
-sensor_msgs::CameraInfo camera_info;
+TisCameraManager *cam;
 
 ros::Publisher image_raw_pub_;
 ros::Publisher camera_info_pub_;
+ros::Publisher image_rect_pub_;
 
 std_msgs::Header header_;
+image_geometry::PinholeCameraModel *pinhole_model_;
+camera_info_manager::CameraInfoManager *camera_info_manager_;
+sensor_msgs::Image img_raw_;
+sensor_msgs::CameraInfo camera_info;
+cv_bridge::CvImage* cv_bridge_img_rect_;
 
-std::string camera_n_, camera_sn_, camera_info_url_;
-int fps_;
+std::string format_, camera_n_, camera_sn_, camera_info_url_;
+int width_, height_, fps_, set_exposure_time_, exposure_lower_limit_, exposure_upper_limit_, exposure_auto_reference_, gain_, gamma_, tonemapping_intensity_;
+double tonemapping_global_brightness_;
+bool exposure_auto_, exposure_limits_, tonemapping_, highlight_reduction_, debugFlag_, propertyFlag_;
+
+#define ListPropertiesDebug 1
+#define ximagesink_display 0
--- src/tis_camera.cpp
+++ src/tis_camera.cpp
@@ -1,43 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <unistd.h>
-#include <string>
-
-#include "tiscamera_interface/tiscamera_interface.hpp"
-
-#include <ros/ros.h>
-
-#include <sensor_msgs/Image.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <sensor_msgs/SetCameraInfo.h>
-
-#include <image_transport/image_transport.h>
-#include <image_transport/camera_publisher.h>
-#include <image_geometry/pinhole_camera_model.h>
-#include <camera_info_manager/camera_info_manager.h>
-
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
-
-TisCameraManager *cam;
-
-image_transport::CameraPublisher img_raw_pub_;
-image_geometry::PinholeCameraModel *pinhole_model_;
-camera_info_manager::CameraInfoManager *camera_info_manager_;
-sensor_msgs::CameraInfo camera_info;
-
-ros::Publisher image_raw_pub_;
-ros::Publisher camera_info_pub_;
-
-std_msgs::Header header_;
-
-std::string format_, camera_n_, camera_sn_, camera_info_url_;
-int width_, height_, fps_;
-bool tonemapping_, highlight_reduction_;
-
-#define ListPropertiesDebug 0
-#define ximagesink_display 0
+#include <tis_ros.h>
 
 void ListProperties(gsttcam::TcamCamera &cam)
 {
@@ -63,28 +24,38 @@ void imageCallback(std::shared_ptr<TisCameraManager::FrameData> data)
         ROS_ERROR("No data received");
         return;
     }
-    // pinhole_model_->fromCameraInfo(camera_info_manager_->getCameraInfo());
     auto datainfo = data->get_info();
-    // std::cout << datainfo.frame_count << "taken." << std::endl;
+    header_.stamp = ros::Time::now();
 
-    cv::Mat OpenCVImage;
-    OpenCVImage.create(datainfo.height, datainfo.width, CV_8UC3);
-    memcpy(OpenCVImage.data, data->image_data(), datainfo.bytes_per_pixel * datainfo.height * datainfo.width);
-    cv_bridge::CvImage img_bridge;
-    sensor_msgs::Image img_msg;
+    img_raw_.header = header_;
+    img_raw_.height = datainfo.height;
+    img_raw_.width = datainfo.width;
+    img_raw_.step = datainfo.bytes_per_pixel * datainfo.width;
+    img_raw_.encoding = sensor_msgs::image_encodings::BGR8;
+    img_raw_.data.resize(img_raw_.height * img_raw_.step);
+    memcpy(&img_raw_.data[0], data->image_data(), img_raw_.height * img_raw_.step);
 
-    header_.seq++;                    
-    header_.stamp = ros::Time::now(); 
-    img_bridge = cv_bridge::CvImage(header_, sensor_msgs::image_encodings::BGR8, OpenCVImage);
-    img_bridge.toImageMsg(img_msg);  
-    image_raw_pub_.publish(img_msg); 
+    if (image_raw_pub_.getNumSubscribers() > 0)
+        image_raw_pub_.publish(img_raw_);
 
     if (camera_info_manager_->isCalibrated())
     {
-        camera_info.header = img_msg.header;
-        camera_info_pub_.publish(camera_info);
+        if (camera_info_pub_.getNumSubscribers() > 0)
+        {
+            camera_info.header = img_raw_.header;
+            camera_info_pub_.publish(camera_info);
+        }
+        if (image_rect_pub_.getNumSubscribers() > 0)
+        {
+            cv_bridge::CvImagePtr cv_img_raw = cv_bridge::toCvCopy(img_raw_, img_raw_.encoding);
+            cv_bridge_img_rect_->header = img_raw_.header;
+            cv_bridge_img_rect_->encoding = img_raw_.encoding;
+            pinhole_model_->fromCameraInfo(camera_info_manager_->getCameraInfo());
+            pinhole_model_->rectifyImage(cv_img_raw->image, cv_bridge_img_rect_->image);
+            image_rect_pub_.publish(*cv_bridge_img_rect_);
+        }
     }
-
+    header_.seq++;
     data->release();
 }
 
@@ -103,13 +74,32 @@ int main(int argc, char **argv)
     }
 
     // Load parameters
-     nh_private.param<std::string>("tis_camera_node/format", format_, "BGR");
+    nh_private.param<bool>("tis_camera_node/debugFlag", debugFlag_, false);
+    nh_private.param<bool>("tis_camera_node/propertyFlag", propertyFlag_, false);
+
     nh_private.param<std::string>("tis_camera_node/camera_name", camera_n_, "tis_front_cam");
     nh_private.param<std::string>("tis_camera_node/camera_serial_number", camera_sn_, "03420356");
+
+    nh_private.param<std::string>("tis_camera_node/format", format_, "BGR");
     nh_private.param<int>("tis_camera_node/width", width_, 1920);
     nh_private.param<int>("tis_camera_node/height", height_, 1080);
     nh_private.param<int>("tis_camera_node/frame_rate", fps_, 10);
+
+    nh_private.param<bool>("tis_camera_node/exposure_auto", exposure_auto_, false);
+    nh_private.param<int>("tis_camera_node/set_exposure_time", set_exposure_time_, 10000);
+    nh_private.param<bool>("tis_camera_node/exposure_limits", exposure_limits_, false);
+    nh_private.param<int>("tis_camera_node/exposure_lower_limit", exposure_lower_limit_, 60);
+    nh_private.param<int>("tis_camera_node/exposure_upper_limit", exposure_upper_limit_, 20000);
+
+    nh_private.param<int>("tis_camera_node/exposure_auto_reference", exposure_auto_reference_, 128);
+
+    nh_private.param<int>("tis_camera_node/gain", gain_, 0);
+    nh_private.param<int>("tis_camera_node/gamma", gamma_, 0);
+
     nh_private.param<bool>("tis_camera_node/tonemapping", tonemapping_, false);
+    nh_private.param<int>("tis_camera_node/tonemapping_intensity", tonemapping_intensity_, 1.0);
+    nh_private.param<double>("tis_camera_node/tonemapping_global_brightness", tonemapping_global_brightness_, 0.5);
+
     nh_private.param<bool>("tis_camera_node/highlight_reduction", highlight_reduction_, false);
 
     header_.frame_id = camera_n_;
@@ -121,6 +111,9 @@ int main(int argc, char **argv)
     {
         camera_info_manager_->loadCameraInfo(camera_info_url_);
         camera_info = camera_info_manager_->getCameraInfo();
+        camera_info.header.frame_id = header_.frame_id;
+        pinhole_model_ = new image_geometry::PinholeCameraModel();
+        cv_bridge_img_rect_ = new cv_bridge::CvImage();
         ROS_INFO_STREAM("Loaded camera calibration from " << camera_info_url_);
     }
     else
@@ -128,8 +121,9 @@ int main(int argc, char **argv)
         ROS_WARN_STREAM("Camera info at: " << camera_info_url_ << " not found. Using an uncalibrated config.");
     }
 
-    image_raw_pub_ = nh_.advertise<sensor_msgs::Image>("sensor/camera/image_raw", fps_);
-    camera_info_pub_ = nh_.advertise<sensor_msgs::CameraInfo>("sensor/camera/camera_info", fps_);
+    image_raw_pub_ = nh_.advertise<sensor_msgs::Image>("sensor/camera/image_raw", 1);
+    camera_info_pub_ = nh_.advertise<sensor_msgs::CameraInfo>("sensor/camera/camera_info", 1);
+    image_rect_pub_ = nh_.advertise<sensor_msgs::Image>("sensor/camera/image_rect", 1);
 
     gst_init(&argc, &argv);
 
@@ -138,27 +132,32 @@ int main(int argc, char **argv)
     ROS_INFO("Starting Camera");
 
     // Set false the ximagesink_display, if no live video display is wanted.
-    if (ximagesink_display) cam->enable_video_display(gst_element_factory_make("xvimagesink", NULL)); // ximagesink
+    if (debugFlag_)
+        cam->enable_video_display(gst_element_factory_make("xvimagesink", NULL)); // ximagesink
 
     // Set a color video format, resolution and frame rate
     cam->set_capture_format(format_, gsttcam::FrameSize{width_, height_}, gsttcam::FrameRate{fps_, 1});
 
     // Set the parameters for the camera
     cam->set_trigger_mode(TisCameraManager::NONE);
-    // cam->set_exposure_gain_auto(true);
-    // cam->set_exposure_auto_reference(128);
-    // cam->set_exposure_time(30000); // in us
-    // cam->set_exposure_auto_reference(true);
-    // cam->set_gain(1);
+    if (exposure_auto_) cam->set_exposure_gain_auto(true);
+    if (!exposure_auto_) cam->set_exposure_time(set_exposure_time_);
+    if (exposure_limits_) cam->set_exposure_limits(exposure_limits_, exposure_lower_limit_, exposure_upper_limit_);
+    if (exposure_auto_reference_) cam->set_exposure_auto_reference(exposure_auto_reference_);
 
     // Start the camera
     cam->start();
 
-    cam->set_tonemapping_mode(tonemapping_);
-    cam->set_highlight_reduction(highlight_reduction_);
+    if (tonemapping_) 
+    {
+        cam->set_tonemapping_mode(tonemapping_);
+        cam->set_tonemapping_param(tonemapping_intensity_, tonemapping_global_brightness_);
+    }
+    if (highlight_reduction_) cam->set_highlight_reduction(highlight_reduction_);
 
     // Set true the ListPropertiesDebug, if you want to see the all camera properties
-    if (ListPropertiesDebug) ListProperties(*cam);
+    if (propertyFlag_)
+        ListProperties(*cam);
 
     cam->registerCallback(imageCallback);
 
@@ -169,7 +168,5 @@ int main(int argc, char **argv)
 
 /*TODO
 - Add some other parameterx ex. exposure, gain, etc.
-- Implement the sub header in config file ex. tonemapping -> intensity etc.
 - Clean and prepare the interface
-- Add the rectified image and publish it
 */
